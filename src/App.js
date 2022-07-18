32f113f2cae4f53309601fc9c163b99c1543eae9
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-07-18 12:19:23 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './component/Home/Navbar/Navbar';
 import Banner from './component/Home/Banner/Banner';
 import LawSections from './component/Home/LawSections/LawSections';
 import Blog from './component/Home/Blog/Blog';
+import Footer from './component/Home/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner></Banner>
       <LawSections></LawSections>
       <Blog></Blog>
+      <Footer></Footer>
     </div>
   );
 }
